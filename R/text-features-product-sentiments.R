fafd3acdbfdaec908699de1823b6482522815fae
Pete Deevakul <editor@mozzarella.website>
2017-01-22 01:56:22 -0500

    plots for word frequencies ~ location
    various subsets and facets

---
+++ R/text-features-product-sentiments.R
@@ -0,0 +1,16 @@
+# Black Market Cannabis Analysis
+# Text Features: Product Listings
+# Sentiment Analysis
+
+# load data -------------------------------------------------------------------
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(tidytext)
+library(stringr)
+library(data.table)
+library(scales)
+
+
+
